d58158c196957659134c2df14a7f865ff3208f55
proxrustes <proxrustes@gmail.com>
2024-01-03 17:41:08 +0100

    UI: index page

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,43 @@ function GET() {
   <html>
   <head>
       <title>HTML Page</title>
+      <style>
+        body {
+          font-family: 'Arial', sans-serif;
+          background-color: #f0f0f0;
+          margin: 0;
+          padding: 10px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+        }
+        .container {
+          text-align: center;
+          background-color: white;
+          padding: 40px;
+          border-radius: 10px;
+          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+          color: #333;
+          font-size: 84px;
+          margin-bottom: 0;
+          margin-top: 0;
+        }
+        h2 {
+          color: #333;
+          font-size: 24px;
+          margin-top: 0;
+        }
+      </style>
   </head>
   <body>
-      <h1>Hello, this is Lab 2!</h1>
+      <div class="container">
+        <h1>Node.JS!</h1>
+        <h2>KUDRIAVTSEVA ANASTASIIA</h2>
+
+      </div>
   </body>
   </html>
 `);
